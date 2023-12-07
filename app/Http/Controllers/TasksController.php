33bd6361b28c6db381aa848cfe5a1ed2c4ea8885
batikankarakan <batikankarakan@gmail.com>
2023-12-08 01:26:29 +0300

    create api request functions

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function auth()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.baubuddy.de/index.php/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+        "username":"365",
+        "password":"1"
+}',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz',
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $decoded = json_decode($response, TRUE);
+        return $decoded["oauth"]["access_token"];
+    }
+
+    public function fetchTasks()
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.baubuddy.de/dev/index.php/v1/tasks/select',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer a9d306bef12392d9f116f8796fee9d92cf492ef8',
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $decoded = json_decode($response, TRUE);
+
+        $tasks = [];
+        foreach ($decoded as $task) {
+            $tasks[] = [
+                "task" => $task["task"],
+                "title" => $task["title"],
+                "description" => $task["description"],
+                "colorCode" => $task["colorCode"],
+            ];
+        }
+
+        return view('tasks', ['tasks' => $tasks]);
+    }
+}
