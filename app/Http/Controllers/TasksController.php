0675e90e7e7a84716986b025dc3c3ef623df33d0
batikankarakan <batikankarakan@gmail.com>
2023-12-09 03:07:28 +0300

    change curl requests to guzzle

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -9,32 +10,17 @@ class TasksController extends Controller
 {
     public function auth()
     {
-        $curl = curl_init();
+        $client = new Client(['base_uri' => 'https://api.baubuddy.de/index.php/']);
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.baubuddy.de/index.php/login',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => '{
-        "username":"365",
-        "password":"1"
-}',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz',
-                'Content-Type: application/json'
-            ),
-        ));
+        $headers = [
+            "Authorization" => "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
+            "Content-type" => "application/json"
+        ];
 
-        $response = curl_exec($curl);
+        $response = $client->request('POST', 'login', ['headers' => $headers, 'body' => '{"username":"365","password":"1"}']);
+        $content = $response->getBody()->getContents();
+        $decoded = json_decode($content, TRUE);
 
-        curl_close($curl);
-
-        $decoded = json_decode($response, TRUE);
         return $decoded["oauth"];
     }
 
@@ -46,26 +32,16 @@ class TasksController extends Controller
             $accessToken = $oAuth["access_token"];
             Cache::put('accessToken', $oAuth["access_token"], $oAuth["expires_in"]);
         }
-        $curl = curl_init();
+        $client = new Client(['base_uri' => 'https://api.baubuddy.de/dev/index.php/v1/tasks/']);
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.baubuddy.de/dev/index.php/v1/tasks/select',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer ' . $accessToken,
-            ),
-        ));
+        $headers = [
+            "Authorization" => "Bearer $accessToken",
+            "Content-type" => "application/json"
+        ];
 
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        $decoded = json_decode($response, TRUE);
+        $response = $client->request('GET', 'select', ['headers' => $headers]);
+        $content = $response->getBody()->getContents();
+        $decoded = json_decode($content, TRUE);
 
         $tasks = [];
         foreach ($decoded as $task) {
@@ -79,7 +55,8 @@ class TasksController extends Controller
         return $tasks;
     }
 
-    public function tasks(){
+    public function tasks()
+    {
         return view('tasks');
     }
 }
